c5cc5ec0516ef9e74471c772f478f36564850f79
Shree Krishna (Consultant) <shrekris@qti.qualcomm.com>
2021-05-28 23:47:58 +0530

    Added Humidity details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ request.get({ url: weatherStackURL, json: true }, (error, { body }) => {
         callback(body.error.info, undefined);
     } else {
         const current = body.current;
-        callback(undefined, current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike + " degrees out.")
+        callback(undefined, current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike + " degrees out. The humidity is " +  current.humidity + "%."  );
     }
 });}
 
