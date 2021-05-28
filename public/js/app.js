ffc5e448ae11db0ff3d7249a56e62543a7f212bc
Shree Krishna (Consultant) <shrekris@qti.qualcomm.com>
2021-05-28 23:29:23 +0530

    Setup App for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,6 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1"); //error
 const messageTwo = document.querySelector("#message-2"); //other changes
 
-// messageOne.textContent = "From JavaScript 1";
-// messageTwo.textContent = "From JavaScript 2";
-
 weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const location = search.value;
@@ -13,7 +10,7 @@ weatherForm.addEventListener("submit", (e) => {
     messageOne.textContent = "Loading...";
     messageTwo.textContent = "";
 
-    fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+    fetch("/weather?address=" + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
