134a1d1690b8283615489c57217b21a5e1de6fdf
Shree Krishna (Consultant) <shrekris@qti.qualcomm.com>
2021-05-28 18:25:03 +0530

    Minor Changes completed

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1"); //error
-const messageTwo = document.querySelector("#message-2"); //other messages
+const messageTwo = document.querySelector("#message-2"); //other changes
 
 // messageOne.textContent = "From JavaScript 1";
 // messageTwo.textContent = "From JavaScript 2";
